d4351bd4b0c54d8595718e05e4d04e009bb5f17c
Kim-Robin <robin1265@gmail.com>
2020-10-13 19:58:49 -0400

    sprinf security added and jwt added

--- Backend/src/main/java/com/PenguinGangT2/Backend/controller/LoginController.java
+++ Backend/src/main/java/com/PenguinGangT2/Backend/controller/LoginController.java
@@ -7,10 +7,16 @@ import java.util.List;
 import com.PenguinGangT2.Backend.exception.ResourceNotFoundException;
 import com.PenguinGangT2.Backend.models.User;
 import com.PenguinGangT2.Backend.repository.UserRepository;
+import com.PenguinGangT2.Backend.service.MyUserDetailsService;
+import com.PenguinGangT2.Backend.util.JwtUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,8 +33,18 @@ public class LoginController {
     @Autowired
     private UserRepository userRepo;
 
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+
     @PostMapping("/signin")
-    public String login(@RequestParam( name = "email") String email, @RequestParam(name = "password") String password){
+    public String login(@RequestParam( name = "email") String email, @RequestParam(name = "password") String password) throws Exception{
         System.out.println(userRepo.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException()));
         User user = userRepo.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException());
         System.out.println(user.getPassword());
@@ -36,8 +52,27 @@ public class LoginController {
         // StandardPasswordEncoder encoder = new StandardPasswordEncoder("secret");
         // System.out.println(encoder.matches(password, user.getPassword()));
         if(user.getPassword().equals(password)){
-            return "Success";
+            try{
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), password));
+            }catch(BadCredentialsException e){
+                throw new Exception("Incorrect username or password", e);
+            }
+            final UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+            final String jwt = jwtUtil.generateToken(userDetails);
+            System.out.println(jwt);
+            return jwt;
         }else{
+            // System.out.println("false");
+            // try{
+            //     authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), password));
+            // }catch(BadCredentialsException e){
+            //     System.out.println(e);
+            //     throw new Exception("Incorrect username or password", e);
+            // }
+            //     final UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+            //     final String jwt = jwtUtil.generateToken(userDetails);
+            //     System.out.println(jwt);
+            //     return jwt;
             return "False";
         }
         
--- Backend/src/main/java/com/PenguinGangT2/Backend/security/SecurityConfig.java
+++ Backend/src/main/java/com/PenguinGangT2/Backend/security/SecurityConfig.java
@@ -1,14 +1,32 @@
 package com.PenguinGangT2.Backend.security;
 
+import com.PenguinGangT2.Backend.service.MyUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-// @EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/login/**").permitAll()
+                .anyRequest().authenticated();
         
         // http.authorizeRequests()
         //     .anyRequest().authenticated()
@@ -17,4 +35,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //     .loginPage("/login")
         //     .permitAll();
     }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
