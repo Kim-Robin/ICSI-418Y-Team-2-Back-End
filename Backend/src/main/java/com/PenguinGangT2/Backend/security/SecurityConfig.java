eca7dec519d0feb7b0c004cceb820c4dc1e1bcfc
Kim-Robin <robin1265@gmail.com>
2020-10-13 22:33:06 -0400

    authentication function is ready ro use

--- Backend/src/main/java/com/PenguinGangT2/Backend/security/SecurityConfig.java
+++ Backend/src/main/java/com/PenguinGangT2/Backend/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.PenguinGangT2.Backend.security;
 
+import com.PenguinGangT2.Backend.filters.JwtRequestFilter;
 import com.PenguinGangT2.Backend.service.MyUserDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +10,18 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter JwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
@@ -26,8 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/login/**").permitAll()
-                .anyRequest().authenticated();
-        
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(JwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         // http.authorizeRequests()
         //     .anyRequest().authenticated()
         //     .and()
